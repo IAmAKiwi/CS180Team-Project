f4ad18312dd8ff4baa95cb19e705bc5f8942e5e5
IAmAKiwi <jackson.shields.college@gmail.com>
2024-11-01 13:17:33 -0400

    Created MessageTestCases and slightly adjusted Message and Database to reformat the toString to a more legible format.

--- Database.java
+++ Database.java
@@ -172,7 +172,6 @@ public class Database implements DatabaseInterface {
     }
 
     public boolean loadMessages() {
-        // TODO: read backup file into allChats
         File messagesFile = new File("messageHistory.txt");
         if (!messagesFile.exists()) {
             return false;
@@ -200,7 +199,7 @@ public class Database implements DatabaseInterface {
                     line = line + "\n" + br.readLine();
                 }
                 // Creates a new Message and adds it to the list
-                Message m = new Message(line.substring(line.indexOf(':') + 1, line.length() - 1),
+                Message m = new Message(line.substring(line.indexOf(' ') + 1, line.length() - 1),
                         line.substring(0, line.indexOf(':')));
                 messages.add(m);
                 line = br.readLine();
