9dff4bd890fb5c7e3f3498c4dd5c85876da034a1
Buiilding <peterbuics@gmail.com>
2024-11-09 10:43:29 -0500

    update

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client implements Runnable {
@@ -17,34 +20,42 @@ public class Client implements Runnable {
 
     }
 
-    public String requestData(String command) throws IOException
-    {
+    public boolean sendMessage(String content) {
+        try {
+            String command = "sendMessage," + content;
+            serverWriter.write(command);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String requestData(String command) throws IOException {
         serverWriter.println(command);
         serverWriter.flush();
         return serverReader.readLine();
     }
 
-    public boolean boolCommand(String command) throws IOException
-    {
+    public boolean boolCommand(String command) throws IOException {
         return Boolean.parseBoolean(this.requestData(command).trim()); // The trim is key.
     }
 
-    // IMPORTANT!! Command format is lowercase command name, colon (:), arguments seperated by semicolons (username;password).
+    // IMPORTANT!! Command format is lowercase command name, colon (:), arguments
+    // seperated by semicolons (username;password).
     // NO SPACES!!
-    // In addition, use the above functions to send and read the commands. Abstraction is good.
-    //TODO: format all remaining methods in this fashion
+    // In addition, use the above functions to send and read the commands.
+    // Abstraction is good.
+    // TODO: format all remaining methods in this fashion
 
-    public boolean login(String username, String password)
-    {
-        try
-        {
+    public boolean receivelogin(String username, String password) {
+        try {
             return boolCommand(String.format(
-                "login:%s;%s", username, password));
-        } catch (IOException ioe)
-        {
+                    "login:%s;%s", username, password));
+        } catch (IOException ioe) {
             return false;
         }
     }
+
     public static void main(String[] args) {
         Thread t = new Thread(new Client());
         t.start();
--- src/main/java/ClientInterface.java
+++ src/main/java/ClientInterface.java
@@ -1,3 +1,58 @@
-public interface ClientInterface {
+import java.io.IOException;
 
+public interface ClientInterface {
+    void run();
+
+    String requestData(String command) throws IOException;
+
+    boolean boolCommand(String command) throws IOException;
+
+    boolean login(String username, String password);
+
+    // when sending message, GUI takes that data and send to client, client sends to
+    // server, server stores in database, server also send that content to another
+    // client
+    boolean receiveMessage();
+
+    // When receiving the message, server sends content to the receiver's client and
+    // client sends data to GUI
+    boolean sendMessage();
+
+    boolean removeFriend();
+
+    boolean addFriend();
+
+    boolean unblockUser();
+
+    boolean deleteMessage();
+
+    boolean sendPhoto();
+
+    boolean accessingProfile();
+
+    boolean updateProfile();
+
+    boolean requestActive();
+
+    boolean deleteChat();
+
+    public boolean sendMessage(String otherUsername);
+
+    public boolean sendImage(String otherUsername, String image);
+
+    public boolean openChat(String otherUsername);
+
+    public String[] getFriendList();
+
+    public boolean addBlock(String otherUsername);
+
+    public String[] getBlockList();
+
+    public boolean isFriendsOnly(String otherUsername);
+
+    public boolean setFriendsOnly(boolean friendsOnly);
+
+    public boolean setProfilePic(String profilePic);
+
+    public boolean logout();
 }
